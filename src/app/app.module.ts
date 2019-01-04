2007b9efb80255014d492c124e968a161eba2d21
Rodrigo Oliveira <rodrigooliveira@rodrigo.home>
2019-01-04 08:56:22 -0200

    setup inicial de componentes e styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContextMenuComponent } from './components/context-menu/context-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContextMenuComponent
   ],
   imports: [
     BrowserModule
